744cb8a76d6a655198687ddc57fca3161824fd3a
Tanya Jurecki <tljurecki@gmail.com>
2022-07-02 11:43:17 -0400

    finished serveice worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -25,5 +25,38 @@ self.addEventListener('install', function (e) {
             return cache.addAll(FILES_TO_CACHE)
         })
     )
-})
+});
 
+self.addEventListener('activate', function(e) {
+    e.waitUntil(
+        caches.keys().then(function(keyList) {
+            let cacheKeepList = keyList.filter(function(key) {
+                return key.indexOf(APP_PREFIX);
+            });
+            cacheKeepList.push(CACHE_NAME);
+            
+            return Promise.all(keyList.map(function (key, i) {
+                if (cacheKeepList.indexOf(key) === -1) {
+                    console.log('deleting cache : ' + keyList[i] );
+                    return caches.delete(keyList[i]);
+                }
+            }));
+
+        })
+    )
+});
+
+self.addEventListener('fetch', function(e) {
+    console.log('fetch request : ' + e.request.url)
+    e.respondWith(
+        cacches.match(e.request).then(function (request) {
+            if (request) { //if cache available respond with cache
+                console.log('responding with cache : ' + e.request.url)
+                return request
+            } else { //if no cache try fetching request
+                console.log('file is not cached, fetching : ' + e.request.url)
+                return fetch(e.request)
+            }
+        })
+    )
+});
